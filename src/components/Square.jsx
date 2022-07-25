a8b32de2dceb24974372eba7968f78ae84056c4e
sahilkumar0000 <9899982913aa@gmail.com>
2022-07-25 10:29:25 +0530

    added event tictactoe is working

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const square = ({ value }) => {
+const square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
